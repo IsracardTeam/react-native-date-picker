eb63b5c1ed3d44c7c0f9e377c3312e9e62b14b5f
IsracardTeam <33055665+IsracardTeam@users.noreply.github.com>
2021-01-11 21:45:08 +0200

    Update MonthWheel.java
    Mispelled CENTER (CENGER)

--- android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
+++ android/src/main/java/com/henninghall/date_picker/wheels/MonthWheel.java
@@ -44,7 +44,7 @@ public class MonthWheel extends Wheel
 
     @Override
     public Paint.Align getTextAlign() {
-        return Paint.Align.CENGER;
+        return Paint.Align.CENTER;
     }
 
     @Override
